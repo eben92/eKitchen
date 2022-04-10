b0a683c5beb437e20958100b23934b3d0c4eb62e
Eben <pappy.rhutmannjnrolege@gmail.com>
2022-04-10 10:11:25 +0000

    ui fixes

--- foods.php
+++ foods.php
@@ -1,27 +1,26 @@
+<?php include('partials-front/menu.php'); ?>
 
-    <?php include('partials-front/menu.php'); ?>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+ <div class="container">
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
-                <input type="search" name="search" placeholder="Search for Food.." required>
-                <input type="submit" name="submit" value="Search" class="btn btn-primary">
-            </form>
+  <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
+   <input type="search" name="search" placeholder="Search for Food.." required>
+   <input type="submit" name="submit" value="Search" class="btn btn-primary">
+  </form>
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
-
+ </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
 
 
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
 
-            <?php 
+<!-- fOOD MEnu Section Starts Here -->
+<section class="food-menu">
+ <div class="container">
+  <h2 class="text-center">Food Menu</h2>
+  <div class="food-search-box-container">
+   <?php 
                 //Display Foods that are Active
                 $sql = "SELECT * FROM tbl_food WHERE active='Yes'";
 
@@ -44,10 +43,10 @@
                         $price = $row['price'];
                         $image_name = $row['image_name'];
                         ?>
-                        
-                        <div class="food-menu-box">
-                            <div class="food-menu-img">
-                                <?php 
+
+   <div class="food-menu-box">
+    <div class="food-menu-img food-search-container">
+     <?php 
                                     //CHeck whether image available or not
                                     if($image_name=="")
                                     {
@@ -58,26 +57,32 @@
                                     {
                                         //Image Available
                                         ?>
-                                        <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                                        <?php
+     <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive image-s img-curve mr-3">
+     <?php
                                     }
                                 ?>
-                                
-                            </div>
 
-                            <div class="food-menu-desc">
-                                <h4><?php echo $title; ?></h4>
-                                <p class="food-price">$<?php echo $price; ?></p>
-                                <p class="food-detail">
-                                    <?php echo $description; ?>
-                                </p>
-                                <br>
 
-                                <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
-                            </div>
-                        </div>
 
-                        <?php
+     <div class="food-menu-desc">
+      <h4><?php echo $title; ?></h4>
+      <div class="d-flex">
+       <p class="food-price">GH&#8373; <?php echo $price; ?></p>
+      </div>
+      <p class="food-detail">
+       <?php echo $description; ?>
+      </p>
+     </div>
+     <br>
+
+    </div>
+
+    <div class="mt-4 d-flex justify-content-center">
+     <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="order-now bg-dark w-100 text-center">Order Now</a>
+    </div>
+   </div>
+
+   <?php
                     }
                 }
                 else
@@ -86,18 +91,18 @@
                     echo "<div class='error'>Food not found.</div>";
                 }
             ?>
+  </div>
 
-            
 
-            
 
-            <div class="clearfix"></div>
 
-            
+  <div class="clearfix"></div>
 
-        </div>
 
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
 
-    <?php include('partials-front/footer.php'); ?>
+ </div>
+
+</section>
+<!-- fOOD Menu Section Ends Here -->
+
+<?php include('partials-front/footer.php'); ?>
